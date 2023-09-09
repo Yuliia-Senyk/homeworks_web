35ad46d4ee35a586e381ecaae54be946b222eaf4
smazepaa <smazepa@kse.ord.ua>
2023-09-09 22:11:31 +0300

    homework 2

---
+++ scripts/Script1.js
@@ -0,0 +1,21 @@
+function Submitted(){
+        alert('your recipe was successfully uploaded');
+        document.getElementById("hidden-div").removeAttribute("hidden");
+
+        var title = document.getElementById("recipe-title").value;
+        document.getElementById("title-table").innerHTML = title;
+
+        var category = document.getElementById("category").value;
+        document.getElementById("category-table").innerHTML = category;
+
+        var number = document.getElementById("servings").value;
+        document.getElementById("number-table").innerHTML = number;
+
+        var name = document.getElementById("firstname").value;
+        var last = document.getElementById("lastname").value;
+        var full = name + " " + last;
+        document.getElementById("name-table").innerHTML = full;
+
+        var email = document.getElementById("email").value;
+        document.getElementById("email-table").innerHTML = email;
+}
